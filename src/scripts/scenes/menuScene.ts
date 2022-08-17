aebea63c7f38e354bf6e6b37fd20d8886e8c756c
Mihai Vasilache <mvasilache@adobe.com>
2022-08-17 21:32:38 +0300

    anim gif

--- src/scripts/scenes/menuScene.ts
+++ src/scripts/scenes/menuScene.ts
@@ -6,7 +6,7 @@ export default class MenuScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/menu/becgraund-2.gif');
+        this.load.spritesheet('background-ss', 'assets/menu/background.png', { frameWidth: 568, frameHeight: 384 });
         this.load.image('play', 'assets/menu/play-button.png');
         this.load.image('play-focus', 'assets/menu/focus.png');
         this.load.image('title', 'assets/menu/title.png');
@@ -29,27 +29,30 @@ export default class MenuScene extends Phaser.Scene {
         this.cameras.main.fadeIn(1000);
         this.cameras.main.setBackgroundColor('#000000');
 
+        this.anims.create({
+            key: 'background-anim',
+            frames: this.anims.generateFrameNumbers('background-ss', {}),
+            frameRate: 3,
+            repeat: -1
+        });
+
         let screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        let screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
 
-        let background = this.add.sprite(screenCenterX, 540, 'background').setInteractive();
-        background.setOrigin(0.5, 0.5);
-        let title = this.add.image(screenCenterX, 250, 'title');
-        title.setOrigin(0.4, 0.5);
-        let play = this.add.sprite(screenCenterX, 850, 'play').setInteractive();
-        play.setOrigin(0.5, 0.5);
-
+        let background = this.physics.add.sprite(screenCenterX, screenCenterY, 'background-ss', 0);
+        background.anims.play('background-anim');
+        background.body.immovable = true;
+        background.body.setAllowGravity(false);
         let scaleX = this.cameras.main.width / background.width;
         let scaleY = this.cameras.main.height / background.height;
         let scale = Math.max(scaleX, scaleY);
         background.setScale(scale);
-        let scaleX1 = this.cameras.main.width / title.width;
-        let scaleY1 = this.cameras.main.height / title.height;
-        let scale1 = Math.max(15, 15);
-        title.setScale(scale1);
-        let scaleX2 = this.cameras.main.width / play.width;
-        let scaleY2 = this.cameras.main.height / play.height;
-        let scale2 = Math.min(20, 20);
-        play.setScale(scale2);
+
+        let title = this.add.image(screenCenterX, 250, 'title');
+        title.setScale(15);
+
+        let play = this.add.sprite(screenCenterX, 850, 'play').setInteractive();
+        play.setScale(20);
 
         play.on(Phaser.Input.Events.POINTER_OVER, () => {
             play.setTexture('play-focus');
--- src/scripts/sprites/hero.ts
+++ src/scripts/sprites/hero.ts
@@ -1,24 +1,25 @@
 import 'phaser';
 
 enum HeroPosition {
-    WEST,
-    EAST,
-    NORTH,
-    SOUTH 
+    WEST,
+    EAST,
+    NORTH,
+    SOUTH
 }
 enum HeroState {
-    IDLE,
-    WALK,
-    ATTACK,
-    DEAD
+    IDLE,
+    WALK,
+    ATTACK,
+    DEAD
 }
 
 export default class Hero extends Phaser.GameObjects.Sprite {
-    keyLeft: Phaser.Input.Keyboard.Key;
-    keyRight: Phaser.Input.Keyboard.Key;
-    keyUp: Phaser.Input.Keyboard.Key;
-    keyDown: Phaser.Input.Keyboard.Key;
-    keyFire: Phaser.Input.Keyboard.Key;
+    keyLeft: Phaser.Input.Keyboard.Key;
+    keyRight: Phaser.Input.Keyboard.Key;
+    keyUp: Phaser.Input.Keyboard.Key;
+    keyDown: Phaser.Input.Keyboard.Key;
+    keyFire: Phaser.Input.Keyboard.Key;
 
-    heroState: HeroState = HeroState.IDLE;
-    heroPosition: HeroPosition = HeroPosition.WEST;
+    heroState: HeroState = HeroState.IDLE;
+    heroPosition: HeroPosition = HeroPosition.WEST;
+}
