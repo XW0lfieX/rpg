d4a808d0b3549dac412ffc5f2080e8e3d4e0d9de
Mihai Vasilache <mvasilache@adobe.com>
2022-08-18 09:02:47 +0300

    movement+attack

--- src/scripts/scenes/gameScene.ts
+++ src/scripts/scenes/gameScene.ts
@@ -1,21 +1,40 @@
 import 'phaser';
 import { Scene } from 'phaser';
+import Hero from '../sprites/hero';
 
 export default class GameScene extends Scene {
+    hero: Hero;
+
     constructor() {
         super({ key: 'GameScene' });
     }
 
     preload() {
-        this.load.spritesheet('char_attack_down-ss', 'assets/hero/char_attack_down_anim_strip_6.png', { frameWidth: 48, frameHeight: 32 });
-        this.load.spritesheet('char_attack_right-ss', 'assets/hero/char_attack_right_anim_strip_6.png', { frameWidth: 48, frameHeight: 32 });
-        this.load.spritesheet('char_attack_left-ss', 'assets/hero/char_attack_left_anim_strip_6.png', { frameWidth: 32, frameHeight: 48 });
-        this.load.spritesheet('char_attack_right-ss', 'assets/hero/char_attack_right_anim_strip_6.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('hero-atk-s-ss', 'assets/hero/char_attack_down_anim_strip_6.png', { frameWidth: 48, frameHeight: 32 });
+        this.load.spritesheet('hero-atk-e-ss', 'assets/hero/char_attack_right_anim_strip_6.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('hero-atk-w-ss', 'assets/hero/char_attack_left_anim_strip_6.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('hero-atk-n-ss', 'assets/hero/char_attack_up_anim_strip_6.png', { frameWidth: 48, frameHeight: 32 });
+
+        this.load.spritesheet('hero-idle-s-ss', 'assets/hero/char_idle_down_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('hero-idle-w-ss', 'assets/hero/char_idle_left_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('hero-idle-e-ss', 'assets/hero/char_idle_right_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('hero-idle-n-ss', 'assets/hero/char_idle_up_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+
+        this.load.spritesheet('hero-walk-s-ss', 'assets/hero/char_run_down_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('hero-walk-w-ss', 'assets/hero/char_run_left_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('hero-walk-e-ss', 'assets/hero/char_run_right_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('hero-walk-n-ss', 'assets/hero/char_run_up_anim_strip_6.png', { frameWidth: 16, frameHeight: 16 });
+
+        this.load.spritesheet('hero-death-ss', 'assets/hero/char_death_all_dir_anim_strip_10.png', { frameWidth: 16, frameHeight: 16 });
     }
 
     create() {
         this.cameras.main.fadeIn(1000);
-        this.cameras.main.setBackgroundColor('#000000');
+        this.cameras.main.setBackgroundColor('#FF0000');
+
+        this.hero = new Hero(this, 100, 100);
+
+        this.physics.world.setBoundsCollision(true, true, true, true);
     }
 
     update(time, delta) {}
--- src/scripts/sprites/hero.ts
+++ src/scripts/sprites/hero.ts
@@ -21,5 +21,218 @@ export default class Hero extends Phaser.GameObjects.Sprite {
     keyFire: Phaser.Input.Keyboard.Key;
 
     heroState: HeroState = HeroState.IDLE;
-    heroPosition: HeroPosition = HeroPosition.WEST;
+    heroPosition: HeroPosition = HeroPosition.EAST;
+
+    constructor(scene, x, y) {
+        super(scene, x, y, 'char_idle_left-ss', 0);
+
+        //idle
+        this.anims.create({
+            key: 'hero-idle-e-anim',
+            frames: this.anims.generateFrameNumbers('hero-idle-e-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'hero-idle-w-anim',
+            frames: this.anims.generateFrameNumbers('hero-idle-w-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'hero-idle-n-anim',
+            frames: this.anims.generateFrameNumbers('hero-idle-n-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'hero-idle-s-anim',
+            frames: this.anims.generateFrameNumbers('hero-idle-s-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        //walk
+        this.anims.create({
+            key: 'hero-walk-e-anim',
+            frames: this.anims.generateFrameNumbers('hero-walk-e-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'hero-walk-w-anim',
+            frames: this.anims.generateFrameNumbers('hero-walk-w-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'hero-walk-n-anim',
+            frames: this.anims.generateFrameNumbers('hero-walk-n-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'hero-walk-s-anim',
+            frames: this.anims.generateFrameNumbers('hero-walk-s-ss', {}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        //attack
+        this.anims.create({
+            key: 'hero-atk-e-anim',
+            frames: this.anims.generateFrameNumbers('hero-atk-e-ss', {}),
+            frameRate: 10,
+            repeat: 0
+        });
+        this.anims.create({
+            key: 'hero-atk-w-anim',
+            frames: this.anims.generateFrameNumbers('hero-atk-w-ss', {}),
+            frameRate: 10,
+            repeat: 0
+        });
+        this.anims.create({
+            key: 'hero-atk-n-anim',
+            frames: this.anims.generateFrameNumbers('hero-atk-n-ss', {}),
+            frameRate: 10,
+            repeat: 0
+        });
+        this.anims.create({
+            key: 'hero-atk-s-anim',
+            frames: this.anims.generateFrameNumbers('hero-atk-s-ss', {}),
+            frameRate: 10,
+            repeat: 0
+        });
+
+        //death
+        this.anims.create({
+            key: 'hero-death-anim',
+            frames: this.anims.generateFrameNumbers('hero-death-ss', {}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.keyLeft = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyRight = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.keyUp = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.keyDown = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keyFire = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+
+        (this.body as Phaser.Physics.Arcade.Body).setSize(10, 14);
+        (this.body as Phaser.Physics.Arcade.Body).setOffset(2, 0);
+        (this.body as Phaser.Physics.Arcade.Body).setCollideWorldBounds(true);
+
+        this.setScale(10);
+    }
+
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+
+        (this.body as Phaser.Physics.Arcade.Body).setVelocity(0);
+
+        if (this.heroState == HeroState.DEAD) {
+            return;
+        }
+
+        if (this.keyFire.isDown && this.heroState != HeroState.ATTACK) {
+            let cardinalPosition = HeroPosition[this.heroPosition].charAt(0).toLowerCase();
+
+            this.anims.play('hero-atk-' + cardinalPosition + '-anim');
+            console.log(this.anims.getName());
+            this.heroState = HeroState.ATTACK;
+            this.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+                this.heroState = HeroState.IDLE;
+                this.anims.play('hero-idle-' + cardinalPosition + '-anim');
+            });
+        }
+
+        //correct bag images
+        if (this.heroState == HeroState.ATTACK && this.heroPosition == HeroPosition.EAST) {
+            this.setOrigin(0.25, 0.5);
+            (this.body as Phaser.Physics.Arcade.Body).setOffset(2, 16);
+        }
+        if (this.heroState == HeroState.ATTACK && this.heroPosition == HeroPosition.WEST) {
+            this.setOrigin(0.75, 0.5);
+            (this.body as Phaser.Physics.Arcade.Body).setOffset(18, 16);
+        }
+        if (this.heroState == HeroState.ATTACK && this.heroPosition == HeroPosition.NORTH) {
+            this.setOrigin(0.5, 0.75);
+        }
+        if (this.heroState == HeroState.ATTACK && this.heroPosition == HeroPosition.SOUTH) {
+            this.setOrigin(0.5, 0.25);
+        }
+
+        if (this.heroState == HeroState.ATTACK) {
+            return;
+        }
+
+        //non attack
+        (this.body as Phaser.Physics.Arcade.Body).setSize(10, 14);
+        (this.body as Phaser.Physics.Arcade.Body).setOffset(2, 0);
+        this.setOrigin(0.5, 0.5);
+
+        if (this.keyRight.isDown) {
+            (this.body as Phaser.Physics.Arcade.Body).setVelocityX(175);
+            this.heroState = HeroState.WALK;
+            this.heroPosition = HeroPosition.EAST;
+        }
+
+        if (this.keyLeft.isDown) {
+            (this.body as Phaser.Physics.Arcade.Body).setVelocityX(-175);
+            this.heroState = HeroState.WALK;
+            this.heroPosition = HeroPosition.WEST;
+        }
+
+        if (this.keyDown.isDown) {
+            (this.body as Phaser.Physics.Arcade.Body).setVelocityY(175);
+            this.heroState = HeroState.WALK;
+            this.heroPosition = HeroPosition.SOUTH;
+        }
+        if (this.keyUp.isDown) {
+            (this.body as Phaser.Physics.Arcade.Body).setVelocityY(-175);
+            this.heroState = HeroState.WALK;
+            this.heroPosition = HeroPosition.NORTH;
+        }
+
+        //hero is idle
+        if (this.keyRight.isUp && this.keyLeft.isUp && this.keyDown.isUp && this.keyUp.isUp) {
+            this.heroState = HeroState.IDLE;
+        }
+
+        //animations
+        if (this.heroState == HeroState.IDLE) {
+            if (this.heroPosition == HeroPosition.EAST) {
+                this.anims.play('hero-idle-e-anim', true);
+            }
+            if (this.heroPosition == HeroPosition.WEST) {
+                this.anims.play('hero-idle-w-anim', true);
+            }
+            if (this.heroPosition == HeroPosition.SOUTH) {
+                this.anims.play('hero-idle-s-anim', true);
+            }
+            if (this.heroPosition == HeroPosition.NORTH) {
+                this.anims.play('hero-idle-n-anim', true);
+            }
+        }
+        if (this.heroState == HeroState.WALK) {
+            if (this.heroPosition == HeroPosition.EAST) {
+                this.anims.play('hero-walk-e-anim', true);
+            }
+            if (this.heroPosition == HeroPosition.WEST) {
+                this.anims.play('hero-walk-w-anim', true);
+            }
+            if (this.heroPosition == HeroPosition.SOUTH) {
+                this.anims.play('hero-walk-s-anim', true);
+            }
+            if (this.heroPosition == HeroPosition.NORTH) {
+                this.anims.play('hero-walk-n-anim', true);
+            }
+        }
+
+        // Normalize and scale the velocity so that this.hero can't move faster along a diagonal
+        (this.body as Phaser.Physics.Arcade.Body).velocity.normalize().scale(175);
+    }
 }
